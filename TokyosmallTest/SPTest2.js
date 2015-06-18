a7d89938a6fe888556f85b4547bba0f408bf7baf
Theresa Becker <theresa.becker@colorado.edu>
2015-06-18 12:03:19 -0600

    testing json

--- TokyosmallTest/SPTest2.js
+++ TokyosmallTest/SPTest2.js
@@ -13,7 +13,7 @@ This demo gives examples of
   $(document).ready(function(){
 
    var graphP = $.ajax({
-    url: 'https://rawgit.com/theresajbecker/CompBio/master/TokyosmallTest/CYJSSmallNode.json',
+    url: 'https://rawgit.com/theresajbecker/CompBio/master/TokyosmallTest/TestData/DICTTEST.json',
     type: 'GET',
     dataType: 'json'
   }).done(function(graphP) {  
@@ -33,21 +33,21 @@ This demo gives examples of
       style: cytoscape.stylesheet()
         .selector('node')
           .css({
-            'content': 'data(id)',
-            'font-size' : 'mapData(weight, 0, 100, 0, 10)',
-            'height': 'mapData(weight, 0, 100, 10, 50)',
-            'width': 'mapData(weight, 0, 100, 10, 50)',
+            'content': 'data(source)',
+            'font-size' : 'mapData(total_mutations, 0, 100, 0, 10)',
+            'height': 'mapData(total_mutations, 0, 100, 10, 50)',
+            'width': 'mapData(total_mutations, 0, 100, 10, 50)',
             
             'color' : 'black',
             //node opacity 
-            'background-opacity': 'mapData(weight, 0, 100, 0, 1)',
-            'border-opacity': 'mapData(weight, 0, 100, 0, 1)',
+            'background-opacity': 'mapData(total_mutations, 0, 100, 0, 1)',
+            'border-opacity': 'mapData(total_mutations, 0, 100, 0, 1)',
             //text-opacity is for both background and outline
-            'text-opacity': 'mapData(weight, 0, 100, .5, 1)',
+            'text-opacity': 'mapData(total_mutations, 0, 100, .5, 1)',
             'text-valign': 'center',
             'text-outline-width': 1,
             'text-outline-color': 'black',
-            'background-color': 'mapData(weight, 40, 100, yellow, red)',
+            'background-color': 'mapData(total_mutations, 40, 100, yellow, red)',
       })
     .selector(':selected')
       .css({
@@ -56,8 +56,8 @@ This demo gives examples of
       })
     .selector('edge')
       .css({
-        'opacity': 'mapData(weight, 0, 100, .5, 1)',
-        'width': 'mapData(weight, 0, 100, 0, 3)',
+        'opacity': 'mapData(total_mutations, 0, 100, .5, 1)',
+        'width': 'mapData(total_mutations, 0, 100, 0, 3)',
         'line-color': 'black',
       }),
       layout: {
